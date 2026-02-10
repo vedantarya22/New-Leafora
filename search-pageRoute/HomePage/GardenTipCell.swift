c0f9fd59a8bcbb909d2fe20981010a5faca268d7
krish4245 <krishna.27.5.2005@gmail.com>
2026-02-10 14:50:10 +0530

    reviewafter

---
+++ search-pageRoute/HomePage/GardenTipCell.swift
@@ -0,0 +1,17 @@
+//
+//  Gardentipcell.swift
+//  search-pageRoute
+//
+//  Created by SDC-USER on 10/02/26.
+//
+
+import UIKit
+
+class Gardentipcell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
