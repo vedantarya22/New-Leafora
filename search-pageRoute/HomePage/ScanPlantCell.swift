2df03204f26873f9dd4c4c5492f39cdb6a08dee0
krish4245 <krishna.27.5.2005@gmail.com>
2026-02-11 10:54:03 +0530

    SCANPLANT OPTION UI

---
+++ search-pageRoute/HomePage/ScanPlantCell.swift
@@ -0,0 +1,17 @@
+//
+//  ScanPlantCell.swift
+//  search-pageRoute
+//
+//  Created by SDC-USER on 11/02/26.
+//
+
+import UIKit
+
+class ScanPlantCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
